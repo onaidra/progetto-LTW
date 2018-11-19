5c9ec5a4440a3d24b3965fa3e292dbda4a030d78
onaidra <44979383+onaidra@users.noreply.github.com>
2018-11-19 11:20:39 +0100

    Add files via upload

---
+++ SignIn2.js
@@ -0,0 +1,9 @@
+// toggle password visibility
+$("#oye").on("click", function() {
+    $(this).toggleClass("glyphicon-eye-close");
+      var type = $("#password2").attr("type");
+    if (type == "text"){ 
+      $("#password2").prop('type','password');}
+    else{ 
+      $("#password2").prop('type','text'); }
+    });
