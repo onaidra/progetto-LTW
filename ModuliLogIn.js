ae1fd39a2fe73e6890b470d458f7d677517e65ee
flaminiapapa <44979670+flaminiapapa@users.noreply.github.com>
2018-11-21 16:42:54 +0100

    Add files via upload

---
+++ ModuliLogIn.js
@@ -0,0 +1,10 @@
+function validaForm() {
+    if (document.registr.name.value=="") {
+    alert("Insert Username");
+    return false;
+    }
+    if (document.registr.password.value=="") {
+        alert("Insert password");
+        return false;
+        }
+    }
